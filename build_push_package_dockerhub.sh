8b4e8a73fdd7d1b44df77bb372a7adbe2e607c11
Michal Duda <github@vookimedlo.cz>
2022-09-24 16:58:59 +0200

    🏗Build: Initial qt6 support

--- build_push_package_dockerhub.sh
+++ build_push_package_dockerhub.sh
@@ -12,80 +12,53 @@ set -e
 # GCC
 #
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/debian-gcc:gcc_stable                       gcc/debian/stable
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/debian-gcc:gcc_buster                       gcc/debian/buster
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/debian-gcc:gcc_bullseye                     gcc/debian/bullseye
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-gcc:gcc_bionic                       gcc/ubuntu/bionic
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-gcc:gcc_xenial                       gcc/ubuntu/xenial
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-gcc:gcc_focal                        gcc/ubuntu/focal
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-gcc:gcc_hirsute                      gcc/ubuntu/hirsute
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-gcc:gcc_impish                       gcc/ubuntu/impish
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/debian-gcc:gcc_bookworm                     gcc/debian/bookworm
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-gcc:gcc_jammy                        gcc/ubuntu/jammy
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-gcc:gcc_kinetic                      gcc/ubuntu/kinetic
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-gcc:gcc_stable                       gcc/fedora/stable
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-gcc:gcc_34                           gcc/fedora/34
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-gcc:gcc_35                           gcc/fedora/35
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-gcc:gcc_36                           gcc/fedora/36
 docker buildx build --progress plain --push --platform linux/amd64                           -t vookimedlo/alpine-gcc:gcc_latest                       gcc/alpine/latest
 
 # Clang
 #
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/debian-clang:clang_stable                   clang/debian/stable
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/debian-clang:clang_buster                   clang/debian/buster
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/debian-clang:clang_bullseye                 clang/debian/bullseye
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-clang:clang_bionic                   clang/ubuntu/bionic
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-clang:clang_xenial                   clang/ubuntu/xenial
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-clang:clang_focal                    clang/ubuntu/focal
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-clang:clang_hirsute                  clang/ubuntu/hirsute
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-clang:clang_impish                   clang/ubuntu/impish
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/debian-clang:clang_bookworm                 clang/debian/bookworm
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-clang:clang_jammy                    clang/ubuntu/jammy
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-clang:clang_kinetic                  clang/ubuntu/kinetic
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-clang:clang_stable                   clang/fedora/stable
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-clang:clang_34                       clang/fedora/34
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-clang:clang_35                       clang/fedora/35
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-clang:clang_36                       clang/fedora/36
 docker buildx build --progress plain --push --platform linux/amd64                           -t vookimedlo/alpine-clang:clang_latest                   clang/alpine/latest
 
 
 # Alpine - distro Qt - just gcc. Clang seems to be broken
 #
-docker buildx build --progress plain --push --platform linux/amd64                           -t vookimedlo/alpine-qt:latestDistroOfficial_gcc_stable   qt/alpine/latest/latestDistroOfficial/gcc
+docker buildx build --progress plain --push --platform linux/amd64                           -t vookimedlo/alpine-qt:qt6_latestDistroOfficial_gcc_stable     qt/alpine/latest/latestDistroOfficial/gcc
 
 
 # Debian - distro Qt
 #
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/debian-qt:latestDistroOfficial_clang_stable   qt/debian/stable/latestDistroOfficial/clang
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/debian-qt:latestDistroOfficial_gcc_stable     qt/debian/stable/latestDistroOfficial/gcc
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/debian-qt:latestDistroOfficial_clang_buster   qt/debian/buster/latestDistroOfficial/clang
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/debian-qt:latestDistroOfficial_gcc_buster     qt/debian/buster/latestDistroOfficial/gcc
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/debian-qt:latestDistroOfficial_clang_bullseye qt/debian/bullseye/latestDistroOfficial/clang
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/debian-qt:latestDistroOfficial_gcc_bullseye   qt/debian/bullseye/latestDistroOfficial/gcc
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/debian-qt:qt6_latestDistroOfficial_clang_stable   qt/debian/stable/latestDistroOfficial/clang
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/debian-qt:qt6_latestDistroOfficial_gcc_stable     qt/debian/stable/latestDistroOfficial/gcc
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/debian-qt:qt6_latestDistroOfficial_clang_bullseye qt/debian/bullseye/latestDistroOfficial/clang
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/debian-qt:qt6_latestDistroOfficial_gcc_bullseye   qt/debian/bullseye/latestDistroOfficial/gcc
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/debian-qt:qt6_latestDistroOfficial_clang_bookworm qt/debian/bookworm/latestDistroOfficial/clang
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/debian-qt:qt6_latestDistroOfficial_gcc_bookworm   qt/debian/bookworm/latestDistroOfficial/gcc
 
 # Fedora - distro Qt
 #
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-qt:latestDistroOfficial_clang_stable   qt/fedora/stable/latestDistroOfficial/clang
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-qt:latestDistroOfficial_gcc_stable     qt/fedora/stable/latestDistroOfficial/gcc
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-qt:latestDistroOfficial_clang_34       qt/fedora/34/latestDistroOfficial/clang
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-qt:latestDistroOfficial_gcc_34         qt/fedora/34/latestDistroOfficial/gcc
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-qt:latestDistroOfficial_clang_35       qt/fedora/35/latestDistroOfficial/clang
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-qt:latestDistroOfficial_gcc_35         qt/fedora/35/latestDistroOfficial/gcc
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-qt:qt6_latestDistroOfficial_clang_stable   qt/fedora/stable/latestDistroOfficial/clang
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-qt:qt6_latestDistroOfficial_gcc_stable     qt/fedora/stable/latestDistroOfficial/gcc
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-qt:qt6_latestDistroOfficial_clang_36       qt/fedora/36/latestDistroOfficial/clang
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-qt:qt6_latestDistroOfficial_gcc_36         qt/fedora/36/latestDistroOfficial/gcc
 
 # Ubuntu - distro Qt
 #
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-qt:latestDistroOfficial_clang_bionic   qt/ubuntu/bionic/latestDistroOfficial/clang
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-qt:latestDistroOfficial_gcc_bionic     qt/ubuntu/bionic/latestDistroOfficial/gcc
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-qt:latestDistroOfficial_clang_xenial   qt/ubuntu/xenial/latestDistroOfficial/clang
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-qt:latestDistroOfficial_gcc_xenial     qt/ubuntu/xenial/latestDistroOfficial/gcc
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-qt:latestDistroOfficial_clang_focal    qt/ubuntu/focal/latestDistroOfficial/clang
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-qt:latestDistroOfficial_gcc_focal      qt/ubuntu/focal/latestDistroOfficial/gcc
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-qt:latestDistroOfficial_clang_hirsute  qt/ubuntu/hirsute/latestDistroOfficial/clang
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-qt:latestDistroOfficial_gcc_hirsute    qt/ubuntu/hirsute/latestDistroOfficial/gcc
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-qt:latestDistroOfficial_clang_impish   qt/ubuntu/impish/latestDistroOfficial/clang
-docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-qt:latestDistroOfficial_gcc_impish     qt/ubuntu/impish/latestDistroOfficial/gcc
-
-# There are no ARM Qt binaries available for given version of Qt
-#
-docker buildx build --progress plain --push --platform linux/amd64  -t vookimedlo/ubuntu-qt:5.14_clang_xenial  qt/ubuntu/xenial/5.14/clang
-docker buildx build --progress plain --push --platform linux/amd64  -t vookimedlo/ubuntu-qt:5.14_gcc_xenial    qt/ubuntu/xenial/5.14/gcc
-docker buildx build --progress plain --push --platform linux/amd64  -t vookimedlo/ubuntu-qt:5.14_clang_bionic  qt/ubuntu/bionic/5.14/clang
-docker buildx build --progress plain --push --platform linux/amd64  -t vookimedlo/ubuntu-qt:5.14_gcc_bionic    qt/ubuntu/bionic/5.14/gcc
-docker buildx build --progress plain --push --platform linux/amd64  -t vookimedlo/ubuntu-qt:5.14_clang_focal   qt/ubuntu/focal/5.14/clang
-docker buildx build --progress plain --push --platform linux/amd64  -t vookimedlo/ubuntu-qt:5.14_gcc_focal     qt/ubuntu/focal/5.14/gcc
-docker buildx build --progress plain --push --platform linux/amd64  -t vookimedlo/ubuntu-qt:5.15_clang_focal   qt/ubuntu/focal/5.15/clang
-docker buildx build --progress plain --push --platform linux/amd64  -t vookimedlo/ubuntu-qt:5.15_gcc_focal     qt/ubuntu/focal/5.15/gcc
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-qt:qt6_latestDistroOfficial_clang_jammy    qt/ubuntu/jammy/latestDistroOfficial/clang
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-qt:qt6_latestDistroOfficial_gcc_jammy      qt/ubuntu/jammy/latestDistroOfficial/gcc
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-qt:qt6_latestDistroOfficial_clang_kinetic  qt/ubuntu/kinetic/latestDistroOfficial/clang
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm/v7,linux/arm64  -t vookimedlo/ubuntu-qt:qt6_latestDistroOfficial_gcc_kinetic    qt/ubuntu/kinetic/latestDistroOfficial/gcc
 
 echo "!!! DONE !!!"
 

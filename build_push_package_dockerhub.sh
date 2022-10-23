6b42f46dcb8941a39889e3b9494a186830b3d8ad
Michal Duda <github@vookimedlo.cz>
2022-10-23 11:27:33 +0200

    🏗Build: Fedora 37 added

--- build_push_package_dockerhub.sh
+++ build_push_package_dockerhub.sh
@@ -25,6 +25,7 @@ docker buildx build --progress plain --push --platform linux/amd64,linux/arm64  
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm64  -t vookimedlo/ubuntu-gcc:gcc_kinetic                      gcc/ubuntu/kinetic
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-gcc:gcc_stable                       gcc/fedora/stable
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-gcc:gcc_36                           gcc/fedora/36
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-gcc:gcc_37                           gcc/fedora/37
 docker buildx build --progress plain --push --platform linux/amd64                           -t vookimedlo/alpine-gcc:gcc_latest                       gcc/alpine/latest
 
 # Remove all images
@@ -41,6 +42,7 @@ docker buildx build --progress plain --push --platform linux/amd64,linux/arm64  
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm64  -t vookimedlo/ubuntu-clang:clang_kinetic                  clang/ubuntu/kinetic
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-clang:clang_stable                   clang/fedora/stable
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-clang:clang_36                       clang/fedora/36
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-clang:clang_37                       clang/fedora/37
 docker buildx build --progress plain --push --platform linux/amd64                           -t vookimedlo/alpine-clang:clang_latest                   clang/alpine/latest
 
 # Remove all images
@@ -79,6 +81,8 @@ docker buildx build --progress plain --push --platform linux/amd64,linux/arm64  
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-qt:qt6_latestDistroOfficial_gcc_stable     qt/fedora/stable/latestDistroOfficial/gcc
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-qt:qt6_latestDistroOfficial_clang_36       qt/fedora/36/latestDistroOfficial/clang
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-qt:qt6_latestDistroOfficial_gcc_36         qt/fedora/36/latestDistroOfficial/gcc
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-qt:qt6_latestDistroOfficial_clang_37       qt/fedora/37/latestDistroOfficial/clang
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm64               -t vookimedlo/fedora-qt:qt6_latestDistroOfficial_gcc_37         qt/fedora/37/latestDistroOfficial/gcc
 
 # Remove all images
 #
--- build_push_package_github.sh
+++ build_push_package_github.sh
@@ -27,6 +27,7 @@ docker buildx build --progress plain --push --platform linux/amd64,linux/arm64  
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm64  -t ghcr.io/vookimedlo/ubuntu-gcc:gcc_kinetic                      gcc/ubuntu/kinetic
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm64  -t ghcr.io/vookimedlo/fedora-gcc:gcc_stable                       gcc/fedora/stable
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm64  -t ghcr.io/vookimedlo/fedora-gcc:gcc_36                           gcc/fedora/36
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm64  -t ghcr.io/vookimedlo/fedora-gcc:gcc_37                           gcc/fedora/37
 docker buildx build --progress plain --push --platform linux/amd64              -t ghcr.io/vookimedlo/alpine-gcc:gcc_latest                       gcc/alpine/latest
 
 # Remove all images
@@ -43,6 +44,7 @@ docker buildx build --progress plain --push --platform linux/amd64,linux/arm64  
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm64  -t ghcr.io/vookimedlo/ubuntu-clang:clang_kinetic                  clang/ubuntu/kinetic
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm64  -t ghcr.io/vookimedlo/fedora-clang:clang_stable                   clang/fedora/stable
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm64  -t ghcr.io/vookimedlo/fedora-clang:clang_36                       clang/fedora/36
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm64  -t ghcr.io/vookimedlo/fedora-clang:clang_37                       clang/fedora/37
 docker buildx build --progress plain --push --platform linux/amd64              -t ghcr.io/vookimedlo/alpine-clang:clang_latest                   clang/alpine/latest
 
 # Remove all images
@@ -81,6 +83,8 @@ docker buildx build --progress plain --push --platform linux/amd64,linux/arm64  
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm64  -t ghcr.io/vookimedlo/fedora-qt:qt6_latestDistroOfficial_gcc_stable     qt/fedora/stable/latestDistroOfficial/gcc
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm64  -t ghcr.io/vookimedlo/fedora-qt:qt6_latestDistroOfficial_clang_36       qt/fedora/36/latestDistroOfficial/clang
 docker buildx build --progress plain --push --platform linux/amd64,linux/arm64  -t ghcr.io/vookimedlo/fedora-qt:qt6_latestDistroOfficial_gcc_36         qt/fedora/36/latestDistroOfficial/gcc
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm64  -t ghcr.io/vookimedlo/fedora-qt:qt6_latestDistroOfficial_clang_37       qt/fedora/37/latestDistroOfficial/clang
+docker buildx build --progress plain --push --platform linux/amd64,linux/arm64  -t ghcr.io/vookimedlo/fedora-qt:qt6_latestDistroOfficial_gcc_37         qt/fedora/37/latestDistroOfficial/gcc
 
 # Remove all images
 #
